faa44002e78a47d7b9c6416d2f637076b0ac181b
tanaka_yoshiki <tanaka.yoshiki88@gmail.com>
2020-02-18 11:43:46 +0900

    お問合せページ作成

--- order/urls.py
+++ order/urls.py
@@ -5,4 +5,5 @@ app_name = 'order'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
+    path('inquiry/', views.InquiryView.as_view(), name='inquiry'),
 ]
--- order/views.py
+++ order/views.py
@@ -1,5 +1,11 @@
 from django.views import generic
 
+from .forms import InquiryForm
 
 class IndexView(generic.TemplateView):
     template_name = "order/index.html"
+
+
+class InquiryView(generic.FormView):
+    template_name = "order/inquiry.html"
+    form_class = InquiryForm
