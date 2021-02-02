cddb672f96942d93b54f2f9056e53eb083a0b6fe
tanakayoshiki88 <tanaka.yoshiki88@gmail.com>
2021-02-03 02:12:07 +0900

    カート機能改修2 order.views が漏れていたため、プッシュし直し

--- order/views.py
+++ order/views.py
@@ -9,12 +9,13 @@ from django.urls import reverse_lazy
 from django.views import generic
 
 from order.models import Order
-from cart.models import Cart, CartItem
+from cart.models import CartItem
 from item.models import Item
 
 from .forms import ContactForm, OrderUpdateFormSetBySeller
 
 import os
+import math
 
 logger = logging.getLogger(__name__)
 
@@ -46,98 +47,129 @@ class ContactView(generic.FormView):
 class PlacedOrderListView(LoginRequiredMixin, generic.ListView):
     template_name = 'order/placed_order_list.html'
     model = Order
-    paginate_by = 30
+    paginate_by = 5
 
     def get_queryset(self):
-        logger.info("os.environ.get('ALLOWED_HOSTS'):  {0}".format(os.environ.get('ALLOWED_HOSTS')))
-        order_items = Order.objects.filter(buyer=self.request.user).order_by('pk')
+        order_items = Order.objects.filter(buyer=self.request.user).order_by('-pk')
         return order_items
+
+
+def order_confirm(request, total=0, counter=0, cart_items=None):
+    try:
+        cart_items = CartItem.objects.filter(buyer=request.user, is_active=True).order_by('pk')
+        warning_msg = {}
+        for cart_item in cart_items:
+            if cart_item.item.stock > 0:
+                if cart_item.item.stock > cart_item.quantity:
+                    if cart_item.item.including_tax:
+                        total += math.floor(cart_item.item.price * cart_item.quantity)
+                    else:
+                        total += math.floor(cart_item.item.price * cart_item.quantity * __tax_rate__)
+                else:
+                    if cart_item.item.including_tax:
+                        total += math.floor(cart_item.item.price * cart_item.quantity)
+                    else:
+                        total += math.floor(cart_item.item.price * cart_item.quantity * __tax_rate__)
+                    warning_msg[cart_item.item_id] = 'こちらの商品は現在、在庫数が' + str(cart_item.item.stock) + cart_item.item.unit + 'です。'
+            else:
+                if cart_item.item.including_tax:
+                    total += math.floor(cart_item.item.price * cart_item.quantity)
+                else:
+                    total += math.floor(cart_item.item.price * cart_item.quantity * __tax_rate__)
+                warning_msg[cart_item.item_id] = 'こちらの商品は現在、在庫数がありません。'
+    except ObjectDoesNotExist:
+        pass
+    return render(request, 'order_confirm.html', dict(cart_items=cart_items, total=total, warning_msg=warning_msg))
 
 
 def order_create(request):
 
     all_items_total_price = 0
     item_name_quantity_for_sending_email = {}
+    counter = 0
 
     try:
-        cart = Cart.objects.get(cart_id=request.session.session_key)
-        cart_items = CartItem.objects.filter(cart=cart, is_active=True).order_by('-pk')
-
+        cart_items = CartItem.objects.filter(buyer=request.user, is_active=True).order_by('-pk')
         # Orderモデルインスタンス作成
         for cart_item in cart_items:
+            if cart_item.quantity != 0 and cart_item.quantity <= cart_item.item.stock:
+                # Itemモデルのstock（在庫数）フィールドを発注分減らし、is_activeフィールドをFalseに更新
+                item = Item.objects.get(pk=cart_item.item.id)
+                item.stock -= cart_item.quantity
+                cart_item.is_active = False
 
-            # Itemモデルのstock（在庫数）とreserved_stock（引当在庫数）フィールドを発注分減らす
-            item = Item.objects.get(pk=cart_item.item.id)
-            item.stock -= cart_item.quantity
-            item.reserved_stock -= cart_item.quantity
-            
-            item.save()
+                item.save()
 
-            if cart_item.item.including_tax:
-                total_price = (cart_item.item.price * cart_item.quantity)
-                total_unit = cart_item.quantity
-                all_items_total_price += total_price
-            else:
-                total_price = (cart_item.item.price * cart_item.quantity * __tax_rate__)
-                total_unit = cart_item.quantity
-                all_items_total_price += total_price
+                if cart_item.item.including_tax:
+                    total_price = (cart_item.item.price * cart_item.quantity)
+                    total_unit = cart_item.quantity
+                    all_items_total_price += total_price
+                else:
+                    total_price = (cart_item.item.price * cart_item.quantity * __tax_rate__)
+                    total_unit = cart_item.quantity
+                    all_items_total_price += total_price
 
-            item_name_quantity_for_sending_email[cart_item.item.name] = cart_item.quantity
+                item_name_quantity_for_sending_email[cart_item.item.name] = str(cart_item.quantity) + cart_item.item.unit
 
-            order = Order.objects.create(
-                buyer=request.user,
-                seller=cart_item.item.user,
-                cart_id=cart.cart_id,
-                item_id=cart_item.item.pk,
-                name=cart_item.item.name,
-                price=cart_item.item.price,
-                including_tax=cart_item.item.including_tax,
-                quantity=cart_item.quantity,
-                unit=cart_item.item.unit,
-                moq=cart_item.item.moq,
-                spq=cart_item.item.spq,
-                photo=cart_item.item.photo,
-                total_unit=total_unit,
-                total_price=total_price,
-            )
-            order.save()
+                order = Order.objects.create(
+                    buyer=request.user,
+                    seller=cart_item.item.user,
+                    item_id=cart_item.item.pk,
+                    name=cart_item.item.name,
+                    price=cart_item.item.price,
+                    including_tax=cart_item.item.including_tax,
+                    quantity=cart_item.quantity,
+                    unit=cart_item.item.unit,
+                    moq=cart_item.item.moq,
+                    spq=cart_item.item.spq,
+                    photo=cart_item.item.photo,
+                    total_unit=total_unit,
+                    total_price=total_price,
+                )
+                order.save()
+                cart_item.quantity = 0
+                cart_item.save()
+                counter += 1
+
     except ObjectDoesNotExist:
         pass
 
-    messages.success(request, '発注が完了しました。詳細は発注履歴一覧でご覧いただけます。')
+    if counter > 0:
+        messages.success(request, '発注が完了しました。詳細は発注履歴一覧でご覧いただけます。')
 
-    try:
-        # 発注情報メール件名
-        subject = "Juhatchu 発注情報"
-        # 受注情報メールメッセージ
-        message_for_seller = str(request.user)\
-            + "様から発注がありました。\n詳細は、受注一覧より確認いただけます。\n"\
-            + "----------------------------------------\n"\
-            + str("\n"
-                  .join(["{0} - {1}"
-                        .format(key, value) for (key, value) in item_name_quantity_for_sending_email.items()]))
-        # 受注情報メールメッセージfor DEFAULT_FROM_EMAIL
-        message_for_from = str(request.user) \
-                  + "様から発注がありました。\n詳細は、受注一覧より確認いただけます。\n" \
-                  + "----------------------------------------\n" \
-                  + str("\n"
-                        .join(["{0} - {1}"
-                              .format(key, value) for (key, value) in item_name_quantity_for_sending_email.items()]))
+        try:
+            # 発注情報メール件名
+            subject = "Juhatchu 発注情報"
+            # 受注情報メールメッセージ
+            message_for_seller = str(request.user)\
+                + "様から発注がありました。\n詳細は、受注一覧より確認いただけます。\n"\
+                + "----------------------------------------\n"\
+                + str("\n"
+                      .join(["{0} - {1}"
+                            .format(key, value) for (key, value) in item_name_quantity_for_sending_email.items()]))
+            # 受注情報メールメッセージfor DEFAULT_FROM_EMAIL
+            message_for_from = str(request.user) \
+                      + "様から発注がありました。\n詳細は、受注一覧より確認いただけます。\n" \
+                      + "----------------------------------------\n" \
+                      + str("\n"
+                            .join(["{0} - {1}"
+                                  .format(key, value) for (key, value) in item_name_quantity_for_sending_email.items()]))
 
-        # 送信元アドレス
-        from_email = os.environ.get('DEFAULT_FROM_EMAIL')
-        # 送信先アドレス
-        seller = cart_item.item.user.email
-        recipient_list_for_seller = [seller]
-        recipient_list_for_from = [os.environ.get('DEFAULT_FROM_EMAIL')]
-        # メール送信
-        send_mail(subject, message_for_seller, from_email, recipient_list_for_seller)
-        send_mail(subject, message_for_from, from_email, recipient_list_for_from)
-    except ObjectDoesNotExist:
-        pass
-    cart.delete()
+            # 送信元アドレス
+            from_email = os.environ.get('DEFAULT_FROM_EMAIL')
+            # 送信先アドレス
+            seller = cart_item.item.user.email
+            recipient_list_for_seller = [seller]
+            recipient_list_for_from = [os.environ.get('DEFAULT_FROM_EMAIL')]
+            # メール送信
+            send_mail(subject, message_for_seller, from_email, recipient_list_for_seller)
+            send_mail(subject, message_for_from, from_email, recipient_list_for_from)
+        except ObjectDoesNotExist:
+            pass
 
-    return redirect('order:placed_order_list')
+        return redirect('order:placed_order_list')
+
+    return redirect('cart:cart_detail')
 
 
 def received_order_list(request):
